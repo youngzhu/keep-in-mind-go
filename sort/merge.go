e5f75e1aacd7ab1fa94ef6fe79776180a4083352
young-win <young.zhu.2012@gmail.com>
2022-05-13 17:55:39 +0800

    归并排序，自底向上法（bottom-up）

--- sort/merge.go
+++ sort/merge.go
@@ -7,6 +7,8 @@ package sort
 func MergeSort(x []int) {
 	// 这里不考虑效率，使用常见的自顶向下归并法
 	topDown(x)
+
+	//bottomUp(x) // 测试
 }
 
 // 使用一个辅组切片能是算法更简单
@@ -25,21 +27,22 @@ func mergeSort(x []int, aux []int, lo int, hi int) {
 	mid := lo + (hi-lo)/2
 	mergeSort(x, aux, lo, mid)
 	mergeSort(x, aux, mid+1, hi)
-	merge(x, aux, lo, mid, hi)
+	merge(x, aux, lo, mid+1, hi)
 }
 
 // 辅助切片的作用体现在这里
+// 将两个有序的数组合并
 // 通过aux将x[lo...mid]和x[mid+1...hi]合并为有序数组
 // x[lo...mid]和x[mid+1...hi]各自有序
 func merge(x []int, aux []int, lo int, mid int, hi int) {
 	// copy to aux
-	copy(aux, x)
+	copy(aux[lo:hi+1], x[lo:hi+1])
 
 	// merge back to x[]
-	i, j := lo, mid+1 // 双指针
+	i, j := lo, mid // 双指针
 	k := lo
 	// 交叉
-	for i <= mid && j <= hi {
+	for i < mid && j <= hi {
 		if aux[i] > aux[j] {
 			x[k] = aux[j]
 			j++
@@ -51,7 +54,7 @@ func merge(x []int, aux []int, lo int, mid int, hi int) {
 	}
 
 	// 只剩左半边，依次复制
-	for i <= mid {
+	for i < mid {
 		x[k] = aux[i]
 		i++
 		k++
@@ -62,4 +65,33 @@ func merge(x []int, aux []int, lo int, mid int, hi int) {
 		j++
 		k++
 	}
+}
+
+/******************************************************
+自底向上合并法
+跟自顶向下比，少了个排序的过程
+自顶向下：首先要对x[lo...mid]和x[mid+1...hi]排序
+自底向上：将每一个元素看作一个子数组，一步步向上合并，1-1，2-2 ...
+*******************************************************/
+func bottomUp(x []int) {
+	n := len(x)
+	aux := make([]int, n)
+	var subLength = 1 // 子数组的长度，从1开始
+	for ; subLength < n; subLength *= 2 {
+		idx1, idx2 := 0, subLength // 待合并的2部分的起始索引
+		var maxIdx int             // 最大索引（包含）
+		for idx2 < n {
+			maxIdx = min(idx2+subLength-1, n-1)
+			merge(x, aux, idx1, idx2, maxIdx)
+			idx1 = maxIdx + 1
+			idx2 = idx1 + subLength
+		}
+	}
+}
+
+func min(i int, j int) int {
+	if i > j {
+		return j
+	}
+	return i
 }
--- sort/sort_test.go
+++ sort/sort_test.go
@@ -8,6 +8,9 @@ import (
 // array 保证每次排序前都是无序的
 var testcase = [...]int{100, 5, 15, 22, 66, 34, 15, 1, 1, 0}
 
+//var testcase = [...]int{1, 2, 3, 4}
+//var testcase = [...]int{4, 3, 2, 1}
+
 func isSorted(x []int) bool {
 	for i := 1; i < len(x); i++ {
 		if x[i] < x[i-1] {
